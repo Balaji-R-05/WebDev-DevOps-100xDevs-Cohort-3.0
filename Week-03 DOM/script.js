d5e1bd7672b37070e968a687858dd991405c3673
Balaji-R-05 <balajiramu23@gmail.com>
2025-08-18 00:37:31 +0530

    Week 3 class completed

---
+++ Week-03 DOM/script.js
@@ -0,0 +1,13 @@
+const title = document.querySelector('h1');
+console.log(title.innerHTML)
+
+const firstTodo = document.querySelector('h4');
+console.log(firstTodo.innerHTML)
+
+const secondTodo = document.querySelectorAll('h4')[1];
+console.log(secondTodo.innerHTML)
+
+// console.log(document);
+
+const inputEl = document.querySelector('input')
+console.log(inputEl);
